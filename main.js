0db911f59108c874037b2013ccb30953b8bb127f
firzatullahd <firzatullahdwikoramadhan@gmail.com>
2021-04-27 22:40:11 +0700

    init project

---
+++ main.js
@@ -0,0 +1,8 @@
+import './styles/css/app.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello World</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+
+console.log(process.env.NODE_ENV)
